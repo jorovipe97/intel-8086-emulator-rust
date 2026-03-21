3ff10af3e9e8067cdaa86785a5e4e50b84ff419c
Jose Villalobos <jose.villalobos@geniussports.com>
2026-03-21 17:26:43 -0500

    feat: Added support for jle, jbe instruction.

--- src/instructions.rs
+++ src/instructions.rs
@@ -11,6 +11,8 @@ pub enum OperationType {
     Jnz,
     Je,
     Jl,
+    Jle,
+    Jb,
 }
 
 impl OperationType {
@@ -28,6 +30,8 @@ impl Display for OperationType {
             Self::Jnz => write!(f, "jnz"),
             Self::Je => write!(f, "je"),
             Self::Jl => write!(f, "jl"),
+            Self::Jle => write!(f, "jle"),
+            Self::Jb => write!(f, "jb"),
         }
     }
 }
@@ -646,8 +650,6 @@ pub const INSTRUCTION_ENCODINGS_TABLE: &[InstructionEncoding] = &[
             IP_INC,
             // Destination is in the mod operand.
             implicit_d(0),
-            // Makes data 8-bit, as IP increment is 8-bit.
-            implicit_w(0),
         ],
     },
     InstructionEncoding {
@@ -658,18 +660,9 @@ pub const INSTRUCTION_ENCODINGS_TABLE: &[InstructionEncoding] = &[
                 bit_count: 8,
                 value: 0b0111_0100,
             },
-            // Altough this is x86 reference, the jmp instructions has a single destination operand.
-            // See: https://www.felixcloutier.com/x86/jmp
-            // For this case, the target operand specifies a relative offset (a signed displacement relative to the current value of the instruction pointer in the IP register).
-            // A near jump to a relative offset of 8-bits (rel8) is referred to as a short jump. The CS register is not changed on near and short jumps.
-            //
-            // The BitsIpInc is to indicate that the destination operand is an Instruction Pointer Increment
-            // However the actual data is extracted from data.
             IP_INC,
             // Destination is in the mod operand.
             implicit_d(0),
-            // Makes data 8-bit, as IP increment is 8-bit.
-            implicit_w(0),
         ],
     },
     InstructionEncoding {
@@ -680,18 +673,35 @@ pub const INSTRUCTION_ENCODINGS_TABLE: &[InstructionEncoding] = &[
                 bit_count: 8,
                 value: 0b0111_1100,
             },
-            // Altough this is x86 reference, the jmp instructions has a single destination operand.
-            // See: https://www.felixcloutier.com/x86/jmp
-            // For this case, the target operand specifies a relative offset (a signed displacement relative to the current value of the instruction pointer in the IP register).
-            // A near jump to a relative offset of 8-bits (rel8) is referred to as a short jump. The CS register is not changed on near and short jumps.
-            //
-            // The BitsIpInc is to indicate that the destination operand is an Instruction Pointer Increment
-            // However the actual data is extracted from data.
             IP_INC,
             // Destination is in the mod operand.
             implicit_d(0),
-            // Makes data 8-bit, as IP increment is 8-bit.
-            implicit_w(0),
+        ],
+    },
+    InstructionEncoding {
+        op: OperationType::Jle,
+        bits: &[
+            InstructionBits {
+                usage: InstructionBitsUsage::Literal,
+                bit_count: 8,
+                value: 0b0111_1110,
+            },
+            IP_INC,
+            // Destination is in the mod operand.
+            implicit_d(0),
+        ],
+    },
+    InstructionEncoding {
+        op: OperationType::Jb,
+        bits: &[
+            InstructionBits {
+                usage: InstructionBitsUsage::Literal,
+                bit_count: 8,
+                value: 0b0111_1110,
+            },
+            IP_INC,
+            // Destination is in the mod operand.
+            implicit_d(0),
         ],
     },
 ];
